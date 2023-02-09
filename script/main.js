c19efb0d962782b7d919f106d3ace9242ccc56ae
yoonjeongKIM0224 <gnoejnooy@gmail.com>
2023-02-09 17:52:30 +0900

    디자인 및 부족한 컨텐츠 작업 중

--- script/main.js
+++ script/main.js
@@ -2,7 +2,6 @@ const main = document.querySelector('#main');
 const qna = document.querySelector('#qna');
 const result = document.querySelector('#result');
 const endPoint = qnaList.length;
-const select = ['', '', '', '', '', '', '', '', '', '', '', ''];
 
 const selectEI = [];
 const selectSN = [];
@@ -45,10 +44,12 @@ function goNext(qIdx){ // 처음은 qIdx = 0
     let q = document.querySelector('.qBox'); // .qBox
     q.innerHTML = qnaList[qIdx].q; // .qBox의 질문 넣기
 
+    let numBox = document.querySelector('.num-box'); // .qBox
+    numBox.innerText = `${qIdx + 1}/${qnaList.length}`;
+
     // Iteration: 되풀이
     // 변수 i는 qnaList의 qIdx(처음엔 0)번째의 a 안에 있는 배열의 개수만큼 반복
-    for(let i in qnaList[qIdx].a) { 
-        // console.log(i);
+    for(let i in qnaList[qIdx].a) {
         addAnswer(qnaList[qIdx].a[i].answer, qIdx, i); // 답변(a) 버튼들을 만드는 함수
     }
 
@@ -72,11 +73,6 @@ function addAnswer(answerText, qIdx, idx){
         let targetWhich = qnaList[qIdx].which;
         let target = qnaList[qIdx].a[idx].type;
 
-        select[qIdx] = target;
-
-        console.log(targetWhich);
-        console.log(target);
-
         if(targetWhich === 'EI') {
             selectEI.push(target);
         } else if(targetWhich === 'SN') {
@@ -86,10 +82,6 @@ function addAnswer(answerText, qIdx, idx){
         } else if(targetWhich === 'JP') {
             selectJP.push(target);
         }
-
-        //찐 코드: for(let i = 0; i < select.length; i++){
-        //     select[target[i]] += 1;
-        // }
 
         for(let i = 0; i < children.length; i++) {
             children[i].disabled = true;
@@ -120,45 +112,37 @@ function goResult(){
 }
 
 function calResult(){
-    //indexOf: 인덱스값 반환, Math.max: 최대값, ...select: 전개구문(선택한 배열을 펼치게 해 줌)
-    // console.log(selectEI);
-    // console.log(selectSN);
-    // console.log(selectTF);
-    // console.log(selectJP);
+    const EIresul = String(Math.max(...selectEI));
+    const SNresul = String(Math.max(...selectSN));
+    const TFresul = String(Math.max(...selectTF));
+    const JPresul = String(Math.max(...selectJP));
 
-    console.log('EI값: ' + Math.max(...selectEI));
-    console.log('SN값: ' + Math.max(...selectSN));
-    console.log('TF값: ' + Math.max(...selectTF));
-    console.log('JP값: ' + Math.max(...selectJP));
-
-    const EIresul = Math.max(...selectEI);
-    const SNresul = Math.max(...selectSN);
-    const TFresul = Math.max(...selectTF);
-    const JPresul = Math.max(...selectJP);
-
-    console.log(typeof EIresul);
-
-    console.log(String(EIresul) + String(SNresul) + String(TFresul) + String(JPresul));
-
-    // let resul = selectEI.indexOf(Math.max(...select));
-
-    // return resul;
+    let resul = EIresul + SNresul + TFresul + JPresul;
+    return resul;
 }
 
 function setResult(){
     let point = calResult(); //calResult의 리턴값
 
-    const resultName = document.querySelector('.resultName'); //결과 제목 요소
-    resultName.innerHTML = infoList[point].name; //결과 제목 요소에 텍스트 넣기
+    for(let i = 0; i < infoList.length; i++) {
+        if(infoList[i].num == point) {
+            point = infoList[i];
+            console.log(point);
+            break;
+        }
+    }
 
-    let resultImg = document.createElement('img'); //img 태그 만들기
-    const imgDiv = document.querySelector('#resultImg'); //만든 img 태그 넣을 부모 요소
-    let imgURL = `img/image-${point}.png`; //동일한 주소 변수 생성
-    resultImg.src = imgURL; //img 주소(src)
-    resultImg.alt = point; //img 대체텍스트(alt)
-    resultImg.classList.add('img-fluid'); //img 클래스
-    imgDiv.appendChild(resultImg); //imgDiv(태그 넣을 부모 요소)에 만든 img 태그 넣기
+    const resultName = document.querySelector('.resultName'); //결과 제목 요소
+    resultName.innerHTML = point.name; //결과 제목 요소에 텍스트 넣기
 
     const resultDesc = document.querySelector('.resultDesc');
-    resultDesc.innerHTML = infoList[point].desc;
+    resultDesc.innerHTML = point.desc;
+    
+    const imgDiv = document.querySelector('#resultImg'); //만든 img 태그 넣을 부모 요소
+    let resultImg = document.createElement('img'); //img 태그 만들기
+    let imgURL = `img/image-${point.name}.png`; //동일한 주소 변수 생성
+    resultImg.src = imgURL; //img 주소(src)
+    resultImg.alt = point.name; //img 대체텍스트(alt)
+    resultImg.classList.add('img-fluid'); //img 클래스
+    imgDiv.appendChild(resultImg); //imgDiv(태그 넣을 부모 요소)에 만든 img 태그 넣기
 }
